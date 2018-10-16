a701421f4b7c370a1d6e81a80cb7ddb867df84e2
Phillip Tran <philliptran@cis.vvc.edu>
2018-10-16 10:44:53 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
-
+#include<string>
 
 using namespace std;
 
+string phrase()
+{
+	cout << "Please input a phrase or word. ";
+	string a = "";
+	getline(cin, a);
+	return a;
+}
+
+int count(string a)
+{
+	int number = 0;
+	for (int i = 0; i < a.length(); i++)
+	{
+		if (a.substr(i,1) == "A" || a.substr(i,1) == "a")
+		{
+			number++;
+		}
+	}
+	cout << "The number of the letter a's in your phrase is: ";
+	return number;
+}
 
 int main()
 {
-    cout << "Hello World"<< endl;
-
-    return 0;
+	cout << count(phrase()) << "." << endl;
+    	return 0;
 }
